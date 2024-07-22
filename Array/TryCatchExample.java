af2dec431d36eeee8611afdbff9a07ce72fc7255
datonefeed <alexftn1182003@gmail.com>
2024-07-22 19:10:26 +0700

    fix : change line 16

--- Array/TryCatchExample.java
+++ Array/TryCatchExample.java
@@ -13,7 +13,7 @@
 //         System.out.println("Chương trình tiếp tục chạy sau khi xử lý lỗi.");
 //     }
 // }
-
+// commit
 
 public class TryCatchExample {
     public static void main(String[] args) {
