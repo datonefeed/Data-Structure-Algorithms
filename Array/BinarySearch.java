18195332af181c79b98ab70cee06c685e961a356
datonefeed <alexftn1182003@gmail.com>
2024-10-11 23:15:39 +0700

    Binary Search Solution

---
+++ Array/BinarySearch.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        System.out.println(binarySearch(arr, 3)); // Output: 2
+    }
+}
