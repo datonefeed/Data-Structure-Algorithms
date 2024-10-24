2352458390a3647c5fbcfdb9f640241449c23f3e
datonefeed <alexftn1182003@gmail.com>
2024-10-24 23:58:29 +0700

    solution: ProductofArrayExceptSelf

--- ProductofArrayExceptSelf.java
+++ ProductofArrayExceptSelf.java
@@ -1,3 +1,20 @@
 public class ProductofArrayExceptSelf {
-
+    class Solution {
+        public int[] productExceptSelf(int[] nums) {
+            int k = nums.length;
+            int sum = 1;
+            int[] myArray = new int[k];
+            for (int i = 0; i < k; i++) {
+                int loc = i;
+                for (int j = 0; j < k; j++) {
+                    if (j != loc) {
+                        sum = sum * nums[j];
+                    }
+                }
+                myArray[i] = sum;
+                sum = 1;
+            }
+            return myArray;
+        }
+    }
 }
